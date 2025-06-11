00771a78b675a280af15d3781cf7a16e453d2b9b
Yash Sonkamble <yash.sonkamble2000@gmail.com>
2025-06-10 17:56:40 -0700

    Problem1 solution

---
+++ Problem1.py
@@ -0,0 +1,34 @@
+"""
+Implemented using the BFS traversal technique discussed during the session. 
+I stored the nodes level by level in the result list and returned the final result at the end.
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+            
+        return result     
